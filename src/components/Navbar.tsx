8fae48f89b80d4137b299170bc8b822017962061
kszuyen <kszuyen@gmail.com>
2023-12-28 17:57:40 +0800

    fix navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,11 +14,12 @@ const Navbar = ({ showButton }: { showButton: boolean }) => {
     <nav id="header" className="top-0 z-30 w-full">
       <div className="container mx-auto mt-0 flex w-full flex-row flex-wrap items-center justify-center py-2">
         <a
-          className="flex flex-grow text-2xl font-bold text-purple-600 lg:text-4xl"
+          className="flex text-2xl font-bold text-purple-600 lg:text-4xl"
           href="/"
         >
           Tindog
         </a>
+        <div className="flex flex-grow"></div>
         {showButton && (
           <button
             className="flex rounded-full bg-gradient-to-r from-blue-300 to-yellow-300 px-2 py-1 text-center text-base font-bold text-white hover:bg-gradient-to-r hover:from-blue-400 hover:to-yellow-400"
