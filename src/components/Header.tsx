cc8056a1ccf3b899cc1d355c88420796f63f569a
kszuyen <kszuyen@gmail.com>
2023-12-30 07:34:47 +0800

    responsive sidebar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,3 @@
-// import Image from "next/image";
 "use client";
 
 import { useState } from "react";
@@ -11,19 +10,16 @@ import { cn } from "@/lib/utils/shadcn";
 
 import ProfileButton from "./ProfileButton";
 
-// import Image from "next/image";
-
-// import Image from "next/image";
-
 export default function Header() {
   const [activeLink, setActiveLink] = useState("/MainPage");
   return (
     // aside is a semantic html tag for side content
-    <aside className="flex h-screen flex-col justify-between px-6 py-6 ">
+    <aside className="flex h-screen  min-h-screen flex-col justify-between px-4 py-2 sm:w-32 md:w-48 lg:w-56 xl:w-64">
+      {/* <aside className="fixed left-0 top-0 flex h-screen flex-col justify-between px-2  [&>*]:my-auto"> */}
       <div className="flex flex-col gap-2">
         <div className="p-2">
           <a
-            className="flex-grow text-2xl font-bold text-purple-600 lg:text-4xl"
+            className="flex-grow  px-4 pt-2 text-2xl font-bold text-purple-600 sm:px-0 md:text-3xl lg:text-4xl"
             href="/MainPage"
           >
             Tindog
@@ -84,7 +80,7 @@ function HeaderButton({
             isActive ? "bg-purple-100" : ""
           } flex w-full items-center gap-4 rounded-full p-2 transition-colors duration-300 group-hover:bg-gray-200 lg:pr-4`}
         >
-          <div className="grid h-[40px] w-[40px] place-items-center">
+          <div className="grid h-[40px] w-[40px] place-items-center px-4  max-lg:flex-grow">
             <Icon
               // now that we defined the interface for Icon, we can pass in the size and strokeWidth props safely
               size={26}
@@ -92,7 +88,10 @@ function HeaderButton({
             />
           </div>
           <span
-            className={cn("text-xl max-lg:hidden", isActive && "font-bold")}
+            className={cn(
+              "flex-grow pr-4 text-xl max-lg:hidden",
+              isActive && "font-bold",
+            )}
           >
             {text}
           </span>
