2be9122353726c8599e1f042fe999fee5e4d24f5
kszuyen <kszuyen@gmail.com>
2023-12-29 16:56:19 +0800

    add icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,6 @@ export default function RootLayout({
   return (
     // 如果沒有加"overflow-x-hidden"，swipe頁面往右滑之後會有bug，有可能會可以往右拖移看到滑出去的卡
     <html lang="en" className="overflow-x-hidden">
-      <Head>
-        {/* <link rel="shortcut icon" href={favicon} /> */}
-        <link rel="icon" href="./favicon.png" />
-      </Head>
       <body className={inter.className}>
         <EdgeStoreProvider>{children}</EdgeStoreProvider>
       </body>
